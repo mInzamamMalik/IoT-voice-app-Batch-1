285fa097674a108b9a994689479518da7619d154
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-11-23 20:54:36 +0500

    in class code

---
+++ javascript/class 1/app.js
@@ -0,0 +1,147 @@
+var name = "mark";
+
+console.log(name);
+
+name = "John"
+
+console.log(name);
+
+var nat;
+
+nat = "pk"
+/////////////////////////////////////////
+
+var weight = 150;
+
+console.log(weight + 25);
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// console.log("newNum",newNum);
+
+
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+
+// console.log("newNum", newNum);
+
+
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+
+// console.log("originalNum: ", originalNum)
+
+// var originalNum = "90";
+// originalNum = "90" + 10;
+
+// console.log("originalNum: ", originalNum)
+
+
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// console.log("hello")
+
+
+
+// var 1stPresident = "Washington"; // NOT LEGAL
+
+
+// var myNameIsInzamam = "john"
+// var mynameisinzamam = "john"
+
+////////////////////////////////////////////////
+
+
+// var popularNumber = 4;
+
+// var popularNumber = 2 + 2;
+
+// console.log(4);
+
+
+// var popularNumber = 12 - 24;
+
+// var popularNumber = 3 * 12;
+
+// var num = 10;
+// var popularNumber = num + 200;
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+
+// var whatsLeftOver = 13 % 5 ;
+// console.log(whatsLeftOver);
+
+
+/////////////////////////////////////////////////
+
+// var num = 5; // 6
+// num++;
+// console.log("num: ", num);
+
+
+// var num = 5;
+// num--;
+// console.log("num: ", num);
+
+
+// var num = 1;
+// var newNum = ++num;
+
+// console.log("new num ",newNum)
+
+
+
+var num1 = 10; // 10
+var num2 = 5; // 10 // 11
+var num3 = 3; // 22 // 23
+var num4 = 23; // 22 
+
+num2 = num1++;
+num1 = num2++;
+num3 = --num4;
+num4 = num3++;
+
+console.log(num1); // 10 11 5
+console.log(num2); //11
+console.log(num3); // 23
+console.log(num4); // 22
+
+
+
+
+08:00 AM to 08:59 AM
+09:00 AM to 09:59 AM
+10:00 AM to 10:59 AM
+11:00 AM to 11:59 AM
+12:00 PM to 12:59 PM
+01:00 PM to 01:59 PM
+02:00 PM to 02:59 PM
+03:00 PM to 03:59 PM
+04:00 PM to 04:59 PM
+05:00 PM to 05:59 PM
+06:00 PM to 06:59 PM
+07:00 PM to 07:59 PM
+08:00 PM to 08:59 PM
+09:00 PM to 09:59 PM
+10:00 PM to 10:59 PM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
