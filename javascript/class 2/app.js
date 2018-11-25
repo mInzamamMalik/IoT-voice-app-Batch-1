2e46b782d88a1970986ed055a047a38494a8e434
Inzamam Malik <malikasinger1@users.noreply.github.com>
2018-11-25 15:55:53 +0500

    Add files via upload

---
+++ javascript/class 2/app.js
@@ -0,0 +1,86 @@
+// var num1 = 5;
+// var num2 = 8;
+
+// console.log(num1++); //5
+// console.log(++num2); // 9
+// console.log(num2--); // 9
+// console.log(--num1); //  5
+
+// console.log(num1);
+// console.log(num2);
+
+
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+// totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+
+
+
+
+// var resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);
+
+
+// console.log("Thanks for your input!");
+
+// var message = "Thanks for your input!";
+// console.log(message);
+
+
+
+// var userName = "Anjum bhai"
+// console.log("Thanks, " + userName + "!");
+
+
+
+
+// var userName = "Anjum bhai"
+// var message = "Thanks, ";
+// var banger = "!";
+// console.log(message + userName + banger);
+
+// console.log("2" + "2");
+
+
+// console.log("2 plus 2 equals " + (2 + 2) + 6) 
+
+
+// var spec = prompt("Your is your name?", "malik");
+
+
+
+// var MalikInput = prompt("please enter a number bw 1 to 10");
+// var AnjumInput = prompt("please enter a number bw 1 to 10");
+
+// if (MalikInput == AnjumInput) {
+//     console.log("Correct!");
+// };
+
+
+var correctAnswer = "Vatican";
+var x = "abc";
+
+if (x === correctAnswer) {
+    console.log("Correct!");
+    console.log("pakistan");
+}
+
+
+
+
+
+
+
+
+
+
+
+
