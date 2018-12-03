2ea0a40802692f088063f70a7d283cbb0f0a0224
Inzamam Malik <malikasinger1@users.noreply.github.com>
2018-12-03 16:40:50 +0500

    Add files via upload

---
+++ javascript/class3/app.js
@@ -0,0 +1,138 @@
+
+
+
+
+
+
+// if (fullName === "Mark" + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + lastName) {
+// if (totalCost === 81.50 + 135) {
+// if (totalCost === materialsCost + 135) {
+// if (totalCost === materialsCost + laborCost) {
+// if (x + y === a - b) {
+
+
+
+
+// var yourTicketNumber = 1234;
+
+// if (yourTicketNumber !== 487208) {
+//     console.log("Better luck next time.");
+
+// }
+
+
+
+// console.log(1 > 0);
+// console.log(0 < 1);
+
+// console.log(1 >= 0);
+// console.log(1 >= 1);
+// console.log(0 <= 1);
+// console.log(1 <= 1);
+
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+
+
+// var x = prompt("Where does the Pope live?");
+
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+// if (x !== "Vatican") {
+//     alert("Wrong answer");
+// }
+
+
+// var x = "karachi"
+
+// if (x === "Vatican") {
+
+//     console.log("Correct!");
+// } 
+// else {
+//     console.log("Wrong answer");
+//     console.log("imp");
+// }
+
+
+
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+// else {
+//     score--;
+//     userIQ = "problematic";
+//     alert("Incorrect");
+// }
+
+
+// var correctAnswer = "Vatican";
+
+// if (x === "ami to nhe dekh rahi hain") {
+//     topiPehenLo()
+// }
+// else if (temp < 20) {
+//     topiPehenLo()
+// }
+// else {
+//     alert("Incorrect");
+// }
+
+
+// var weight = 350;
+// var time = 5;
+
+// if (weight > 300 && time < 6) {
+//     console.log("give invite");
+// }
+// else {
+//     console.log("dont give invite");
+// }
+
+
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// }
+
+// var SAT = 30;
+// var GPA = 2.0;
+// var sport = "football";
+
+// if (SAT > 45 || GPA > 2.5 || sport === "football") {
+//     console.log("Welcome to Bubba State!");
+// }
+// else {
+//     console.log("Have you looked into appliance repair?");
+// }
+
+
+var age = 70;
+var res = "P.k."
+
+if ((age > 65 || age < 21) && res === "U.S.") {
+    console.log("correct")
+} else {
+    console.log("incorrect")
+}
+
+
+
+
+
+
+
+
+
+
