a6d3ad2069723775eccb62a3a7ca5f10802cec53
Anjum Ali <anjumalidsd@gmail.com>
2018-12-24 02:04:23 +0500

    Batch1, Class6

---
+++ javascript/class 6/ch20.js
@@ -0,0 +1,49 @@
+// var num; Naufil Code
+// for (var i=1;i<=10;i++)
+// {
+//     console.log("2 * " + i + " ="+ (2*i));
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+// console.log(fullNames);
+
+// for(console.log("init");  true; console.log("inc/dec"))
+// {
+//     console.log("test");
+// }
+
+
+// var twoDimension = [["KHI", "PECHS", "Inner Road"],
+//                     ["LHR", "MALL", "Milk Shop"],
+//                     ["PESH", "BARA", "Bullets"]];
+
+
+// for (var i = 0; i <= 2; i++) {
+//     for (var j = 0; j <= 2; j++) {
+//         console.log(twoDimension[i][j]);
+//     }
+// }
+
+
+
+
+// console.log(    twoDimension[0][0]);
+// console.log(    twoDimension[0][1]);
+// console.log(    twoDimension[0][2]);
+
+// console.log(    twoDimension[1][0]);
+// console.log(    twoDimension[1][1]);
+// console.log(    twoDimension[1][2]);
+
+// console.log(    twoDimension[2][0]);
+// console.log(    twoDimension[2][1]);
+// console.log(    twoDimension[2][2]);
+
+
---
+++ javascript/class 6/ch21.js
@@ -0,0 +1,7 @@
+var city = "karAchi";
+
+console.log(city.toUpperCase());
+
+var city1 = "LAhORE";
+
+console.log(city1.toLowerCase());
