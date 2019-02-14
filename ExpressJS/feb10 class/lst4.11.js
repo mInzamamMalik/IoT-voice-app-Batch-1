f39621f2600039f939ce84c283a3fe3094fca4e8
Anjum Ali <anjumalidsd@gmail.com>
2019-02-14 20:29:54 +0500

    feb10 class

---
+++ ExpressJS/feb10 class/lst4.11.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var morgan = require("morgan");
+var path = require("path");
+var app = express();
+app.use(morgan("short"));
+
+var staticPath = path.join(__dirname, "static");
+
+app.use(express.static(staticPath));
+
+app.use(function (req, res) {
+    res.status(404);
+    res.send("File not found!");
+});
+
+app.listen(8088, function () {
+    console.log("App started on port 8088");
+});
