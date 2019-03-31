a73255603db705871816693619ee3381e19bde24
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2019-03-31 15:19:46 +0500

    firebase class

---
+++ firebase-class/hello world/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld =
+    functions.https.onRequest((request, response) => {
+       
+        response.send("Hello from Firebase!");
+    });
+
+
+export const helloWorld2 =
+    functions.https.onRequest((request, response) => {
+
+
+        response.send("Hello from Firebase!");
+    });
